10ee530ac13300e11dad00a4318ec1e34b83eee7
Leandro Alan Kim <156946451+lkim94gatech@users.noreply.github.com>
2025-01-14 18:06:27 -0500

    Add files via upload

---
+++ src/student.h
@@ -0,0 +1,13 @@
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+#include "os-sim.h"
+
+/* Function declarations */
+extern void idle(unsigned int cpu_id);
+extern void preempt(unsigned int cpu_id);
+extern void yield(unsigned int cpu_id);
+extern void terminate(unsigned int cpu_id);
+extern void wake_up(pcb_t *process);
+
+#endif /* __STUDENT_H__ */
